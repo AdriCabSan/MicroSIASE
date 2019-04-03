9711a344a8f98eaf955e80107ab7cca5dbf83172
Armin716 <armand716@hotmail.com>
2019-04-03 02:23:38 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/microsiase/MainActivity.kt
+++ app/src/main/java/com/example/microsiase/MainActivity.kt
@@ -1,95 +1,139 @@
 package com.example.microsiase
 
-import android.app.Activity
-import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
-import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
+import androidx.core.widget.NestedScrollView
+import com.example.microsiase.R
+import com.example.microsiase.InputValidation
+import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
-class InputValidation
-/**
- * constructor
- *
- * @param context
- */
-(private val context: Context) {
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val activity = this@MainActivity
+
+    private lateinit var nestedScrollView: NestedScrollView
+
+    private lateinit var textInputLayoutMatricula: TextInputLayout
+    private lateinit var textInputLayoutPassword: TextInputLayout
+
+    private lateinit var textInputEditTextMatricula: TextInputEditText
+    private lateinit var textInputEditTextPassword: TextInputEditText
+
+    private lateinit var appCompatButtonLogin: AppCompatButton
+
+    private lateinit var inputValidation: InputValidation
+
+    private lateinit var spinner: Spinner
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+
+        // hiding the action bar
+        supportActionBar!!.hide()
+
+        // initializing the views
+        initViews()
+
+        // initializing the listeners
+        initListeners()
+
+        // initializing the objects
+        initObjects()
+    }
 
     /**
-     * method to check InputEditText filled .
-     *
-     * @param textInputEditText
-     * @param textInputLayout
-     * @param message
-     * @return
+     * This method is to initialize views
      */
-    fun isInputEditTextFilled(textInputEditText: TextInputEditText, textInputLayout: TextInputLayout, message: String): Boolean {
-        val value = textInputEditText.text.toString().trim()
-        if (value.isEmpty()) {
-            textInputLayout.error = message
-            hideKeyboardFrom(textInputEditText)
-            return false
-        } else {
-            textInputLayout.isErrorEnabled = false
-        }
+    private fun initViews() {
 
-        return true
+        nestedScrollView = findViewById<View>(R.id.nestedScrollView) as NestedScrollView
+
+        textInputLayoutMatricula = findViewById<View>(R.id.textInputLayoutMatricula) as TextInputLayout
+        textInputLayoutPassword = findViewById<View>(R.id.textInputLayoutPassword) as TextInputLayout
+
+        textInputEditTextMatricula = findViewById<View>(R.id.textInputEditTextMatricula) as TextInputEditText
+        textInputEditTextPassword = findViewById<View>(R.id.textInputEditTextPassword) as TextInputEditText
+
+        spinner = findViewById<View>(R.id.spinner) as Spinner
+
+        appCompatButtonLogin = findViewById<View>(R.id.appCompatButtonLogin) as AppCompatButton
     }
 
 
     /**
-     * method to check InputEditText has valid matricula .
-     *
-     * @param textInputEditText
-     * @param textInputLayout
-     * @param message
-     * @return
+     * This method is to initialize listeners
      */
-    fun isInputEditTextMatricula(textInputEditText: TextInputEditText, textInputLayout: TextInputLayout, message: String): Boolean {
-        val value = textInputEditText.text.toString().trim()
+    private fun initListeners() {
 
-        var numeric = value.matches("[0-9]*".toRegex())
-
-        if (value.isEmpty() || !numeric) {
-            textInputLayout.error = message
-            hideKeyboardFrom(textInputEditText)
-            return false
-        } else {
-            textInputLayout.isErrorEnabled = false
-        }
-        return true
+        appCompatButtonLogin!!.setOnClickListener(this)
     }
 
     /**
-     * method to check both InputEditText value matches.
-     *
-     * @param textInputEditText1
-     * @param textInputEditText2
-     * @param textInputLayout
-     * @param message
-     * @return
+     * This method is to initialize objects to be used
      */
-    fun isInputEditTextMatches(textInputEditText1: TextInputEditText, textInputEditText2: TextInputEditText, textInputLayout: TextInputLayout, message: String): Boolean {
-        val value1 = textInputEditText1.text.toString().trim()
-        val value2 = textInputEditText2.text.toString().trim()
-        if (!value1.contentEquals(value2)) {
-            textInputLayout.error = message
-            hideKeyboardFrom(textInputEditText2)
-            return false
-        } else {
-            textInputLayout.isErrorEnabled = false
-        }
-        return true
+    private fun initObjects() {
+        inputValidation = InputValidation(activity)
+
     }
 
     /**
-     * method to Hide keyboard
+     * This implemented method is to listen the click on view
      *
-     * @param view
+     * @param v
      */
-    private fun hideKeyboardFrom(view: View) {
-        val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        imm.hideSoftInputFromWindow(view.windowToken, WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN)
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.appCompatButtonLogin -> verifyFromSQLite()
+        }
+    }
+
+    /**
+     * This method is to validate the input text fields and verify login credentials from SQLite
+     */
+    private fun verifyFromSQLite() {
+
+        if (!inputValidation!!.isInputEditTextFilled(textInputEditTextMatricula!!, textInputLayoutMatricula!!, getString(R.string.error_message_matricula))) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextMatricula(textInputEditTextMatricula!!, textInputLayoutMatricula!!, getString(R.string.error_message_matricula))) {
+            return
+        }
+        if (!inputValidation!!.isInputEditTextFilled(textInputEditTextPassword!!, textInputLayoutPassword!!, getString(R.string.error_message_password))) {
+            return
+        }
+
+        /*Aqui es donde verifica si es correcta la informacion y llamaal Menu Acitviti
+        if (databaseHelper!!.checkUser(textInputEditTextMatricula!!.text.toString().trim { it <= ' ' }, textInputEditTextPassword!!.text.toString().trim { it <= ' ' })) {
+
+
+            val accountsIntent = Intent(activity, MenuActivity::class.java)
+            startActivity(accountsIntent)
+
+
+        }*/ else {
+
+            // Snack Bar to show success message that record is wrong
+            Snackbar.make(nestedScrollView!!, getString(R.string.error_valid_matricula_password), Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    /**
+     * This method is to empty all input edit text
+     */
+    private fun emptyInputEditText() {
+        textInputEditTextMatricula!!.text = null
+        textInputEditTextPassword!!.text = null
     }
 }
+
